2b9a850fe02bb7cd370b394b3f0d700b257a2ef7
Sam Collier <samcollier916@gmail.com>
2025-06-28 23:53:54 +0100

    don't use cmd

--- packages/fmt/build.lua
+++ packages/fmt/build.lua
@@ -1,7 +1,6 @@
 if ob.is_unix() then
-  ob.cmd("mkdir", {"build"});
-  ob.cmd("cmake", {"-B", "build"});
-  ob.cmd("cmake", {"--build", "build", "-t", "fmt"});
+  ob.cmake({"-B", "build"});
+  ob.cmake({"--build", "build", "-t", "fmt"});
   ob.add_package({
 	name = "fmt",
 	type = "lib",
