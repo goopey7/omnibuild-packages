dc4fa4260f26db87d3d9371e89b41a1f1aae84a3
Sam Collier <samcollier916@gmail.com>
2025-06-29 15:15:59 +0100

    spdlog

---
+++ packages/spdlog/build.lua
@@ -0,0 +1,10 @@
+if ob.is_unix() then
+	ob.cmake({ "-B", "build", "-DSPDLOG_BUILD_SHARED=ON" });
+	ob.cmake({ "--build", "build", "-t", "spdlog" });
+	ob.add_package({
+		name = "spdlog",
+		type = "dylib",
+		binary = "./build/libspdlog.so",
+		include_dirs = { "./include" },
+	})
+end
